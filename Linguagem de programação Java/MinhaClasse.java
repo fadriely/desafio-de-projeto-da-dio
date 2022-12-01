1ac0dd218ed3d6a6bb222f94995aae54c36ab9e8
fadriely <ferreiraadriely02@gmail.com>
2022-12-01 01:42:01 -0400

    C:/Program Files/Git/adicionando estudos

---
+++ Linguagem de programação Java/MinhaClasse.java
@@ -0,0 +1,18 @@
+ public class MinhaClasse {
+
+ public static void main(String [] args) {
+
+     String primeiroNome = "Adriely";
+     String segundoNome="Ferreira";
+
+     String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+
+     System.out.println(nomeCompleto);
+ }
+
+ public static String nomeCompleto (String primeiroNome, String segundoNome) {
+     return primeiroNome.concat(" ").concat(segundoNome);
+ }
+
+ }
+
